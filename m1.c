6656b7e5b49a38a861a7a3a45a147912060b7418
Santunu Kaysar <shantanumahin@gmail.com>
2025-10-03 10:46:18 +0600

    Initial commit

---
+++ m1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char *arr[] = {"Kaysar","Mahin", "Santunu",};
+    char *name = "Santunu";
+    int len = sizeof(arr) / sizeof(arr[0]);
+    int left =0;
+    int found =0;
+    int right = len-1;
+    while(left<=right){
+        int mid = left + (right-left)/2;
+        int cmp = strcmp(arr[mid], name);
+        if(cmp == 0){
+            printf("Found: %s index %d /n", arr[mid],mid);
+            found =1;
+            break;
+        }
+        else if(cmp < 0){
+            left = mid +1;
+        }
+        else {
+            right = mid -1;
+        }
+    }
+    if(!found){
+        printf("Not Found /n");
+    }
+    return 0;
+}
